c648848ddc3a055e84f0db0ebe93e5d256b811da
Sneha2630 <158037571+Sneha2630@users.noreply.github.com>
2024-02-05 15:19:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Week1 from './components/week1';
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h2>
+       sneha</h2>
+       <h2>2211cs010390</h2>
+        <h2>group 7</h2>
+        <Week1/>
+      </div>
+      );
+     }
+
+export default App;
---
+++ week1.js
@@ -0,0 +1,28 @@
+import React from "react";
+const MyComponentsWithoutJSX=()=>{
+
+}
+const MyComponentsWithJSX=()=>{
+
+}
+const MyProfile=()=>{
+
+}
+function Week1(){
+    return(
+        <div ClassName="week1">
+        <h1>Displaying my MyComponents without JSX</h1>
+        <MyComponentsWithoutJSX/>
+        <hr/>
+        <h1>Displaying my MyComponents with JSX</h1>
+        <MyComponentsWithJSX/>
+        <hr/>
+        <h1>Display My Profile</h1>
+        <MyProfile/>
+    </div>
+    );
+}
+
+export default Week1;
+        
+    
